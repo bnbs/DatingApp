87a10c27523db340b7507ee3682eb09bcda90d3e
Bruna Nayara B. da Silva <bruna.bnayara@outlook.com>
2018-11-22 12:49:14 -0200

    Added 3rd party components

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'DatingApp-SPA';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.setDecodedToken(token);
+    }
+  }
 }
